0338037a604ee26c3080acfee88a862df66d5765
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-11-25 11:23:32 -0600

    Early implementation of a user's love page

--- app/src/main/java/org/ometa/lovemonster/ui/activities/UserLoveActivity.java
+++ app/src/main/java/org/ometa/lovemonster/ui/activities/UserLoveActivity.java
@@ -1,18 +1,58 @@
 package org.ometa.lovemonster.ui.activities;
 
-import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import org.ometa.lovemonster.R;
+import org.ometa.lovemonster.models.Love;
+import org.ometa.lovemonster.models.User;
+import org.ometa.lovemonster.service.LoveMonsterClient;
+import org.ometa.lovemonster.ui.fragments.LovesListFragment;
+
+import java.util.List;
 
 public class UserLoveActivity extends AppCompatActivity {
-    
+    private LovesListFragment lovesSent;
+    private LovesListFragment lovesReceived;
+    private LoveMonsterClient client;
+    private User user;
+    private int nextPage = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_love);
+        lovesSent = (LovesListFragment) getSupportFragmentManager().findFragmentById(R.id.lovesList);
+        lovesReceived = (LovesListFragment) getSupportFragmentManager().findFragmentById(R.id.lovesList);
+        client = LoveMonsterClient.getInstance();
+        user = (User) getIntent().getParcelableExtra("user");
+
+        getLoves();
+    }
+
+    private void getLoves() {
+        client.retrieveRecentLoves(new LoveMonsterClient.LoveListResponseHandler() {
+            @Override
+            public void onSuccess(@NonNull List<Love> loves, int totalPages) {
+                for (Love love : loves) {
+                    if (love.lovee.username == user.username) {
+                        lovesReceived.addLove(love);
+                    } else if (love.lover.username == user.username) {
+                        lovesSent.addLove(love);
+                    }
+                }
+            }
+
+            @Override
+            public void onFail() {
+                Toast.makeText(getApplicationContext(), "Unable to retrieve loves", Toast.LENGTH_SHORT).show();
+            }
+        }, nextPage, user);
+        nextPage += 1;
     }
 
     @Override
