301c52e165dfaa002dc3a90d6bf632183bfe40d3
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-12-02 19:39:44 -0600

    Always show a love's message, if it exists

--- app/src/main/java/org/ometa/lovemonster/ui/adapters/LoveArrayAdapter.java
+++ app/src/main/java/org/ometa/lovemonster/ui/adapters/LoveArrayAdapter.java
@@ -20,24 +20,6 @@ import java.util.List;
  * Created by bschmeckpeper on 11/18/15.
  */
 public class LoveArrayAdapter extends ArrayAdapter<Love> {
-
-
-    static class VisibilityToggler implements View.OnClickListener {
-        View toToggle;
-
-        public VisibilityToggler(View toToggle) {
-            this.toToggle = toToggle;
-        }
-
-        @Override
-        public void onClick(View v) {
-            if (toToggle.getVisibility() == View.VISIBLE) {
-                toToggle.setVisibility(View.GONE);
-            } else {
-                toToggle.setVisibility(View.VISIBLE);
-            }
-        }
-    }
     static class ViewHolder {
 
         private Context context;
@@ -79,14 +61,12 @@ public class LoveArrayAdapter extends ArrayAdapter<Love> {
             tvReason.setText(love.reason);
 
             if (love.hasMessage()) {
-                tvEllipsis.setVisibility(View.VISIBLE);
                 tvMessage.setText(love.message);
+                tvMessage.setVisibility(View.VISIBLE);
             } else {
-                tvEllipsis.setVisibility(View.GONE);
                 tvMessage.setText("");
+                tvMessage.setVisibility(View.GONE);
             }
-
-            //tvMessage.setVisibility(View.GONE);
         }
 
         private String displayNameFor(User user) {
@@ -123,10 +103,8 @@ public class LoveArrayAdapter extends ArrayAdapter<Love> {
         viewHolder.tvLoveeName = (TextView) view.findViewById(R.id.tvRecipientName);
         viewHolder.tvLoverName = (TextView) view.findViewById(R.id.tvSenderName);
         viewHolder.tvReason = (TextView) view.findViewById(R.id.tvReason);
-        viewHolder.tvEllipsis = (TextView) view.findViewById(R.id.tvElipsis);
         viewHolder.tvMessage = (TextView) view.findViewById(R.id.tvMessage);
         viewHolder.tvTimeAgo = (TextView) view.findViewById(R.id.tvTimeAgo);
-        viewHolder.tvEllipsis.setOnClickListener(new VisibilityToggler(viewHolder.tvMessage));
 
         view.setTag(viewHolder);
 
