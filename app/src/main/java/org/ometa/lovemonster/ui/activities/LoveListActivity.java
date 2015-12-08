48fd0520d7b1c8b8a7f6c475578fd5ed69a5e446
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-12-07 22:39:59 -0600

    Use a callback to reload fragment after creating Love

--- app/src/main/java/org/ometa/lovemonster/ui/activities/LoveListActivity.java
+++ app/src/main/java/org/ometa/lovemonster/ui/activities/LoveListActivity.java
@@ -33,7 +33,7 @@ public class LoveListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_love_list);
         LoveMonsterClient client = LoveMonsterClient.getInstance();
 
-        HomeLoveFragment fragment = (HomeLoveFragment) getSupportFragmentManager().findFragmentById(R.id.lovesList);
+        final HomeLoveFragment fragment = (HomeLoveFragment) getSupportFragmentManager().findFragmentById(R.id.lovesList);
         fragment.setCurrentUser(client.getAuthenticatedUser());
 
         final FloatingActionButton makeLoveButton = (FloatingActionButton) findViewById(R.id.make_love);
@@ -41,9 +41,17 @@ public class LoveListActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 final MakeLoveDialogFragment makeLoveDialogFragment = MakeLoveDialogFragment.newInstance(new User("foo@example.com", ""));
+                makeLoveDialogFragment.setOnSuccessCallback(new MakeLoveDialogFragment.SuccessCallback() {
+                     @Override
+                     public void onSuccess() {
+                         fragment.reloadLoves();
+                     }
+                });
                 makeLoveDialogFragment.show(getFragmentManager(), "makeLoveDialog");
+
             }
         });
+
     }
 
     @Override
